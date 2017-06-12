b8c8b36a33f7b144c6b5333fe7c93376de39ef6c
maximilian183 <max.kwok@gmail.com>
2017-06-12 09:55:30 -0700

    Star Trek Wars!

--- src/starTrekDancer.js
+++ src/starTrekDancer.js
@@ -1,4 +1,10 @@
 var makeStarTrekDancer = function(top, left, timeBetweenSteps) {
+
+  if (window.CROWNEDKING) {
+    alert('Victory is decided!  No one else wants to joing the party.')
+    return false;
+  }
+
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
   makeDancer.call(this, top, left, timeBetweenSteps);
@@ -6,19 +12,19 @@ var makeStarTrekDancer = function(top, left, timeBetweenSteps) {
   var randomNumber = Math.floor(Math.random()*5);
 
   if (randomNumber === 0){
-    this.$node = $('<span id="starTrekDancer" class="starTrekDancer andorian" onclick="makeStarTrekDancer.prototype.sayHello(this)"><img src="img/andorian_white.png" height="50" width="50" ></span>');
+    this.$node = $('<span class="starTrekDancer andorian" onclick="makeStarTrekDancer.prototype.attack(this)"><img src="img/andorian_white.png" width="50" ></span>');
   }
   if (randomNumber === 1){
-    this.$node = $('<span class="starTrekDancer borg" onclick="makeStarTrekDancer.prototype.sayHello(this)"><img src="img/borg_red.png" height="50" width="50" ></span>');
+    this.$node = $('<span class="starTrekDancer borg" onclick="makeStarTrekDancer.prototype.attack(this)"><img src="img/borg_red.png" width="50" ></span>');
   }
   if (randomNumber === 2){
-    this.$node = $('<span class="starTrekDancer klingon" onclick="makeStarTrekDancer.prototype.sayHello(this)"><img src="img/klingon_green.png" height="50" width="50" ></span>');
+    this.$node = $('<span class="starTrekDancer klingon" onclick="makeStarTrekDancer.prototype.attack(this)"><img src="img/klingon_green.png" width="50" ></span>');
   }
   if (randomNumber === 3){
-    this.$node = $('<span class="starTrekDancer person" onclick="makeStarTrekDancer.prototype.sayHello(this)"><img src="img/person_purple.png" height="50" width="50" ></span>');
+    this.$node = $('<span class="starTrekDancer person" onclick="makeStarTrekDancer.prototype.attack(this)"><img src="img/person_purple.png" width="50" ></span>');
   }
   if (randomNumber === 4){
-    this.$node = $('<span class="starTrekDancer vulcan" onclick="makeStarTrekDancer.prototype.sayHello(this)"><img src="img/vulcan_blue.png" height="50" width="50" ></span>');
+    this.$node = $('<span class="starTrekDancer vulcan" onclick="makeStarTrekDancer.prototype.attack(this)"><img src="img/vulcan_blue.png" width="50" ></span>');
   }
 
   this.setPosition(top, left);
@@ -28,15 +34,6 @@ var makeStarTrekDancer = function(top, left, timeBetweenSteps) {
 
 makeStarTrekDancer.prototype = Object.create(makeDancer.prototype);
 makeStarTrekDancer.prototype.constructor = makeStarTrekDancer;
-
-makeStarTrekDancer.prototype.step = function() {
-  // call the old version of step at the beginning of any call to this new version of step
-  //makeDancer.prototype.step.call(this);
-  // toggle() is a jQuery method to show/hide the <span> tag.
-  // See http://api.jquery.com/category/effects/ for this and
-  // other effects you can use on a jQuery-wrapped html tag.
-  //this.$node.toggle();
-};
 
 makeStarTrekDancer.prototype.lineUp = function(whichNode) {
   this.$node = $(whichNode);
@@ -49,33 +46,55 @@ makeStarTrekDancer.prototype.lineUp = function(whichNode) {
     window.dancers[i].css('transform', 'translateY(' + distance + 'px)');
     window.dancers[i].css('-webkit-transform', 'translateY(' + distance + 'px)');
   }
-
 };
 
-makeStarTrekDancer.prototype.sayHello = function(node) {
+makeStarTrekDancer.prototype.attack = function(node) {
 
-  var targetNode = this.findClosest(node);
+  var targetNode = this.findClosestTarget(node);
   //Finding closest node to the node you clicked on
 
-  $targetNode.
+  $(targetNode).addClass('target');
 
-  //Creating a monkey node off window
-  this.$node = $('<span class="monkeyDancer"><img src="img/monkey.png" height="200" width="200" ></span>');
-  console.log($(targetNode).css('top'));
-  this.setPosition($(targetNode).css('top'), 200);
+  //Creating a monkey node
+  this.$node = $('<span class="monkeyDancer" style="top: ' + $(node).css('top') + '; left: ' + $(node).css('left') + '"><img src="img/monkey.png" height="100" width="100"></span>');
 
-  //Appending monkey node to the DOM body
-  $('body').append(this.$node);
+  //Appending monkey node to the DOM body next to the selected node
+  $(this.$node).insertAfter(targetNode);
 
+  //Calculating the path from selectedNode to the targetNode
+  var pathx = parseInt($(targetNode).css('left')) - parseInt($(node).css('left'));
+  var pathy = parseInt($(targetNode).css('top')) - parseInt($(node).css('top'));
+  var path = 'translate(' + pathx + 'px, ' + pathy + 'px)';
 
-  //Transforming the position to collide monkey with cloest node
-  this.$node.css('transform', 'translateX(' + $(targetNode).css('left') + 'px)');
-  console.log(this.$node.css('transform'));
-  this.$node.css('-webkit-transform', 'translateX(' + $(targetNode).css('left') + 'px)');
+  //Transforming the position of monkey to attack closest node
+  this.$node.css('transform', path);
+  this.$node.css('-webkit-transform', path);
+
+  //remove targetNode from existence
+  setTimeout(function(){$(targetNode).remove()}, 1000);
+  for(var i = 0; i < window.dancers.length; i++){
+    //console.log(window.dancers[i]);
+    //console.log(targetNode);
+    if (window.dancers[i] === targetNode){
+      window.dancers.splice(i,1);
+    }
+  }
+
+  //show the victor
+  if (window.dancers.length === 1 && !window.CROWNEDKING){
+    this.lineUp($(node));
+    var type = $(node).attr('class').replace('starTrekDancer ', '');
+    type = type.charAt(0).toUpperCase() + type.slice(1);
+    var victory = '<h1 class="victor">' + type + ' Victory</h1>'
+    $('body').append(victory);
+    window.CROWNEDKING = true;
+
+    $(node).children().eq(0).css('transform', 'scale(3)');
+    $(node).children().eq(0).css('-webkit-transform', 'scale(3)');
+  }
 };
 
-
-makeStarTrekDancer.prototype.findClosest = function(node){
+makeStarTrekDancer.prototype.findClosestTarget = function(node){
   var closestDist = Number.MAX_VALUE;
   var closestNode = null;
 
@@ -89,6 +108,6 @@ makeStarTrekDancer.prototype.findClosest = function(node){
       closestNode = window.dancers[i];
     }
   }
-  console.log(closestNode);
+  //console.log(closestNode);
   return closestNode;
-}
+};
